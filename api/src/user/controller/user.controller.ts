e74e06bea474d23f6fc625bcc5653a4aa2029d21
Simone Tassi <72198236+simonetassi@users.noreply.github.com>
2023-12-19 17:06:14 +0100

    fix history

---
+++ api/src/user/controller/user.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService) { }
+
+    @Post()
+    create(@Body() user: User): Observable<User> {
+        return this.userService.create(user);
+    }
+
+    @Get(':id')
+    findOneBy(@Param() params): Observable<User> {
+        return this.userService.findOneBy(params.id);
+    }
+
+    @Get()
+    findAll(): Observable<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string): Observable<any> {
+        return this.userService.deleteOne(Number(id));
+    }
+
+    @Put(':id')
+    updateOne(@Param('id') id: string, @Body() user: User): Observable<any> {
+        return this.userService.updateOne(Number(id), user);
+    }
+}
