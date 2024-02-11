033b4a792fe6ec9f0e3e909c33297f2bc2667864
Simone Tassi <72198236+simonetassi@users.noreply.github.com>
2024-02-11 15:52:14 +0100

    /api: ExecuteService working

--- api/src/execute/service/execute.service.ts
+++ api/src/execute/service/execute.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { HttpClientFactory } from '@node-wot/binding-http';
+import Servient from '@node-wot/core';
 import { RoutineDto } from 'src/routine/dtos/routine.dto';
 
 @Injectable()
 export class ExecuteService {
     execute(routine: RoutineDto): string {
         console.log(routine);
-        
-        return "ciao";
+        const servient = new Servient();
+        servient.addClientFactory(new HttpClientFactory(null));
+        return eval(routine.tsCode);
     }
 }
