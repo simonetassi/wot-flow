e0b30ebb48ab071f10e6e7b5e961ec706c395c6f
Simone Tassi <72198236+simonetassi@users.noreply.github.com>
2024-03-01 18:59:48 +0100

    /api: remove routine logging during execution

--- api/src/execute/service/execute.service.ts
+++ api/src/execute/service/execute.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { HttpClientFactory } from '@node-wot/binding-http';
+import { CoapClientFactory } from '@node-wot/binding-coap';
+import { MqttClientFactory } from '@node-wot/binding-mqtt';
+import { WebSocketClientFactory } from '@node-wot/binding-websockets';
+
 import Servient from '@node-wot/core';
 import { RoutineDto } from 'src/routine/dtos/routine.dto';
 
 @Injectable()
 export class ExecuteService {
     execute(routine: RoutineDto): string {
-        console.log(routine);
+        // console.log(routine);
         const servient = new Servient();
         servient.addClientFactory(new HttpClientFactory(null));
+        servient.addClientFactory(new CoapClientFactory(null));
+        servient.addClientFactory(new MqttClientFactory());
+        servient.addClientFactory(new WebSocketClientFactory(null));
         return eval(routine.tsCode);
     }
 }
